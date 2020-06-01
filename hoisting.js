658910379b5c17be13bc5185c55e34d20d3bb86f
Callum Wilsmore <cal.wilsmore@btinternet.com>
2020-06-01 20:31:35 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -24,11 +24,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-var name = "Kristin";
+  sayMy();
 
   function sayMy() {
-    console.log(name);
 
+    var name = "Kristin";
+      console.log(name)
   }
-    sayMy();
 }
